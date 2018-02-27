e0b8811dd0f148c103d362d48f20e7e8724aef71
Ava Heinonen <avar.heinonen@gmail.com>
2018-02-27 17:09:12 +0200

    osa 5 tehtavat 1-3

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      blogs: []
+      blogs: [],
+      username: '',
+      password: '',
+      user: null,
+      error: null
     }
   }
 
@@ -14,15 +19,90 @@ class App extends React.Component {
     blogService.getAll().then(blogs =>
       this.setState({ blogs })
     )
+
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      this.setState({user})
+    }
   } 
 
+  login = async (event) => {
+    event.preventDefault()
+    try{
+      const user = await loginService.login({
+        username: this.state.username,
+        password: this.state.password
+      })
+
+      window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user))
+      
+      this.setState({ username: '', password: '', user})
+    } catch (exception){
+      this.setState({
+        error: 'Kayttajatunnus tai salasana virheellinen',
+      })
+      setTimeout(() => {
+        this.setState({ error: null })
+      }, 5000)
+    }
+    
+  }
+
+  handleLoginFieldChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  logout = (event) => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    this.setState({ user: null })
+  }
+
   render() {
-    return (
+    const loginForm = () => (
+    <div>
+      <h2>Kirjaudu</h2>
+      <form onSubmit = {this.login}>
+        <div>
+          Kayttajatunnus
+          <input 
+            type="text"
+            name="username"
+            value={this.state.username}
+            onChange={this.handleLoginFieldChange}
+          />
+        </div>
+        <div>
+          Salasana
+          <input
+            type="password"
+            name="password"
+            value={this.state.password}
+            onChange={this.handleLoginFieldChange}
+          />
+        </div>
+        <button type="submit">kirjaudu</button>
+      </form>
+    </div>
+    )
+    const blogList = () => (
       <div>
+        <p>{this.state.user.name}</p>
+        <button onClick = {this.logout}>
+        kirjaudu ulos
+        </button>
         <h2>blogs</h2>
         {this.state.blogs.map(blog => 
           <Blog key={blog._id} blog={blog}/>
         )}
+
+      </div>
+    )
+    return (
+      <div>
+        {this.state.user === null ?
+          loginForm() :
+          blogList() }
       </div>
     );
   }
