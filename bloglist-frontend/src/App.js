37534ec50cddf586fcab3c191c669aeaddceee30
Ava Heinonen <avar.heinonen@gmail.com>
2018-03-01 12:49:57 +0200

    5.6 done

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -139,7 +139,7 @@ class App extends React.Component {
         </button>
         <h2>blogs</h2>
         {this.state.blogs.map(blog => 
-          <Blog key={blog._id} blog={blog}/>
+          <Blog key={blog._id} title={blog.title} author={blog.author} url={blog.url} likes={blog.likes} user={blog.user.name}/>
         )}
       </div>
     )
--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,57 @@
 import React from 'react'
-const Blog = ({blog}) => (
+
+/*const Blog = ({blog}) => (
   <div>
     {blog.title} {blog.author}
   </div>  
-)
+)*/
+
+class Blog extends React.Component {
+	constructor (props) {
+		super(props)
+		this.state = {
+			showAll: false
+		}
+	}
+
+	toggleVisibility = () => {
+		console.log('click')
+		this.setState({showAll : !this.state.showAll })
+	}
+
+	render() {
+		const hideWhenVisible = { display: this.state.showAll ? 'none': '',
+									paddingTop: 10,
+      								paddingLeft: 2,
+      								border: 'solid',
+      								borderWidth: 1,
+     								 marginBottom: 5
+								}
+		const showWhenVisible = { display: this.state.showAll ? '' : 'none',
+									paddingTop: 10,
+      								paddingLeft: 2,
+      								border: 'solid',
+      								borderWidth: 1,
+     								 marginBottom: 5
+									}
+
+
+		return (
+			<div>
+				<div style = {hideWhenVisible}>
+					<h3 onClick ={this.toggleVisibility}>{this.props.title} {this.props.author}</h3>
+				</div>
+				<div style = {showWhenVisible}>
+					<h3 onClick ={this.toggleVisibility}>{this.props.title} {this.props.author}</h3>
+					<a href="{this.props.url}">{this.props.url}</a>
+					<p>{this.props.likes} likes </p> 
+					<button>like</button>
+					<p>Added by {this.props.user} </p>
+				</div>
+			</div>
+			)
+	}
+
+}
 
 export default Blog
